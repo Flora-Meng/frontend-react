47ece180ca5b0a09d4128f543993e15bae36cdec
Flora <florameng999@gmail.com>
2024-02-05 14:49:14 +1100

    update the home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-import { Nabvar } from './components';
+import { Navbar } from './components';
+import './App.scss';
 
 
 const App = () => {
   return (
     <div className='app'>
-    <Nabvar />
+    <Navbar />
     <Header />
     <About/>
     <Work />
